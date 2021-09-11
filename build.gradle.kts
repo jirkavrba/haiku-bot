0c70e673770cccf827e74f88c96351480223b2e6
Jiří Vrba <jiri@vrba.dev>
2021-09-11 19:34:07 +0200

    Add JDA dependency and connect to Discord gateway on startup

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,15 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven {
+        name = "m2-dv8tion"
+        url = uri("https://m2.dv8tion.net/releases")
+    }
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    implementation("net.dv8tion:JDA:4.3.0_324")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:1.5.21")
 }
 
 tasks.test {
--- src/main/kotlin/bot.kt
+++ src/main/kotlin/bot.kt
@@ -1,5 +1,8 @@
+import net.dv8tion.jda.api.JDABuilder
+import net.dv8tion.jda.api.entities.Activity
 
 fun main(args: Array<String>) {
+    // Require the caller to provide a bot token as the first parameter
     val token = args.firstOrNull() ?: throw IllegalArgumentException(
         """
         Please provide a Discord gateway token as the first program argument!
@@ -7,5 +10,9 @@ fun main(args: Array<String>) {
         """.trimIndent()
     )
 
-    println(token)
+    val jda = JDABuilder.createDefault(token)
+        .setActivity(Activity.watching("for your accidental haikus"))
+        .build()
+
+    jda.awaitReady()
 }
