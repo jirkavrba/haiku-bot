08fca7334dedbf8871c7e3927b914e23625db998
Jiří Vrba <jiri@vrba.dev>
2021-09-11 19:29:27 +0200

    Load bot token on application startup

--- src/main/kotlin/bot.kt
+++ src/main/kotlin/bot.kt
@@ -1,6 +1,11 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
 
-    // Try adding program arguments at Run/Debug configuration
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    val token = args.firstOrNull() ?: throw IllegalArgumentException(
+        """
+        Please provide a Discord gateway token as the first program argument!
+        You can obtain your bot token at https://discord.com/developers/applications
+        """.trimIndent()
+    )
+
+    println(token)
 }
